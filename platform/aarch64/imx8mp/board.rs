8b79f062661466a012c1f14859969dc29159cba3
Baozixu99 <770090546@qq.com>
2026-04-30 16:56:45 +0800

    feat:支持直通5G模组，并禁用掉HDMI

--- platform/aarch64/imx8mp/board.rs
+++ platform/aarch64/imx8mp/board.rs
@@ -26,7 +26,7 @@ pub const ROOT_ZONE_CPUS: u64 = (1 << 0) | (1 << 1);
 
 pub const ROOT_ZONE_NAME: &str = "root-linux";
 
-pub const ROOT_ZONE_MEMORY_REGIONS: [HvConfigMemoryRegion; 8] = [
+pub const ROOT_ZONE_MEMORY_REGIONS: [HvConfigMemoryRegion; 12] = [
     HvConfigMemoryRegion {
         mem_type: MEM_TYPE_RAM,
         physical_start: 0x50000000,
@@ -44,49 +44,66 @@ pub const ROOT_ZONE_MEMORY_REGIONS: [HvConfigMemoryRegion; 8] = [
         physical_start: 0x30c00000,
         virtual_start: 0x30c00000,
         size: 0x400000,
-    },
+    }, // audio/dma
     HvConfigMemoryRegion {
         mem_type: MEM_TYPE_IO,
         physical_start: 0x30800000,
         virtual_start: 0x30800000,
         size: 0x400000,
-    },
+    }, // bus@30800000
     HvConfigMemoryRegion {
         mem_type: MEM_TYPE_IO,
         physical_start: 0x38000000,
         virtual_start: 0x38000000,
         size: 0x8000,
-    },
+    }, // gpu3d
     HvConfigMemoryRegion {
         mem_type: MEM_TYPE_IO,
         physical_start: 0x38008000,
         virtual_start: 0x38008000,
         size: 0x8000,
-    },
+    }, // gpu2d
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
+        physical_start: 0x38100000,
+        virtual_start: 0x38100000,
+        size: 0x10000,
+    }, // usb1 dwc3
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
+        physical_start: 0x38200000,
+        virtual_start: 0x38200000,
+        size: 0x10000,
+    }, // usb2 dwc3
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
+        physical_start: 0x381f0000,
+        virtual_start: 0x381f0000,
+        size: 0x1000,
+    }, // usb-phy@381f0040
+    HvConfigMemoryRegion {
+        mem_type: MEM_TYPE_IO,
+        physical_start: 0x382f0000,
+        virtual_start: 0x382f0000,
+        size: 0x1000,
+    }, // usb-phy@382f0040
     HvConfigMemoryRegion {
         mem_type: MEM_TYPE_IO,
         physical_start: 0x38500000,
         virtual_start: 0x38500000,
         size: 0x20000,
-    },
+    }, // vipsi
     HvConfigMemoryRegion {
         mem_type: MEM_TYPE_IO,
         physical_start: 0x32c00000,
         virtual_start: 0x32c00000,
         size: 0x400000,
     }, // hdmi
-       // bus@30800000
-       // HvConfigMemoryRegion {
-       //     mem_type: MEM_TYPE_IO,
-       //     physical_start: 0x30890000,
-       //     virtual_start: 0x30890000,
-       //     size: 0x1000,
-       // }, // serial
 ];
 
-pub const ROOT_ZONE_IRQS: [u32; 32] = [
-    34,35, 36, 37, 38, 45, 52, 54,55, 56, 57,58, 59, 64, 67, 74,75, 96, 97, 98, 99, 100, 101, 102, 103, 104,
-    105, 135, 150, 151, 152, 162,
+pub const ROOT_ZONE_IRQS: [u32; 36] = [
+    34,35, 36, 37, 38, 45, 52, 54,55, 56, 57,58, 59, 64, 67, 72,73,74,75, 96, 97, 98, 99, 100, 101, 102, 103, 104,
+    105, 135, 150, 151, 152, 162,180,181
 ];
 
 pub const ROOT_ARCH_ZONE_CONFIG: HvArchZoneConfig = HvArchZoneConfig {
